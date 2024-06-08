fa89cd84644531041e7d04d7043553a24b40cca6
Armand <armandwinant@googlemail.com>
2024-06-08 23:40:57 +0200

    create prediction endpoint function

--- deployment/web-service/predict.py
+++ deployment/web-service/predict.py
@@ -1,4 +1,5 @@
 import pickle
+from flask import Flask, request, jsonify
 
 with open('lin_reg.bin', 'rb') as f_in:
   (dv, model) = pickle.load(f_in)
@@ -16,3 +17,15 @@ def predict(features):
   preds = model.predict(X)
 
   return preds[0]
+
+def predict_endpoint():
+  ride = request.get_json()
+
+  features = prepare_features(ride)
+  pred = predict(features)
+
+  result = {
+    'duration': pred
+  }
+
+  return jsonify(result)
