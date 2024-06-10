6968c02a9ff8eea78a901c74bdd2ed836491aaa3
Armand <armandwinant@googlemail.com>
2024-06-10 23:25:21 +0200

    add Dockerfile for containerizing pred script

--- deployment/web-service/predict.py
+++ deployment/web-service/predict.py
@@ -1,37 +1,37 @@
 import pickle
 from flask import Flask, request, jsonify
 
-with open('lin_reg.bin', 'rb') as f_in:
-  (dv, model) = pickle.load(f_in)
-
+with open('lin_reg.bin', 'rb') as f:
+	(dv, model) = pickle.load(f)
 
 def prepare_features(ride):
-  features = {}
-  features['PU_DO'] =  f"{ride['PULocationID']}_{ride['DOLocationID']}"
-  features['trip_distance'] = ride['trip_distance']
+	features = {
+		'PU_DO': f"{ride['PULocationID']}_{ride['DOLocationID']}",
+		'trip_distance': ride['trip_distance']
+	}
 
-  return features
+	return features
 
 def predict(features):
-  X = dv.transform(features)
-  preds = model.predict(X)
+	X = dv.transform([features])
+	preds = model.predict(X)[0]
 
-  return preds[0]
+	return preds
 
 app = Flask('duration-prediction')
 
 @app.route('/predict', methods=['POST'])
 def predict_endpoint():
-  ride = request.get_json()
+	ride = request.get_json()
 
-  features = prepare_features(ride)
-  pred = predict(features)
+	features = prepare_features(ride)
+	pred = predict(features)
 
-  result = {
-    'duration': pred
-  }
+	result = {
+		'duration': pred
+	}
 
-  return jsonify(result)
+	return jsonify(result)
 
 if __name__ == '__main__':
-  app.run(debug=True, host='0.0.0.0', port=9696)
+	app.run(debug=True, host='0.0.0.0', port=9696)
