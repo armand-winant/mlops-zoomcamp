08d4481b61b832e5d860cb1847461161cfc7ccde
Armand <armandwinant@googlemail.com>
2024-06-22 23:55:50 +0200

    use cleaning util in yellow taxi transformer

--- orchestration/mlops/homework_03/transformers/prepare_yellow_taxi.py
+++ orchestration/mlops/homework_03/transformers/prepare_yellow_taxi.py
@@ -1,4 +1,5 @@
 import pandas as pd
+from mlops.utils.data_preparation.cleaning import clean
 
 if 'transformer' not in globals():
     from mage_ai.data_preparation.decorators import transformer
@@ -21,15 +22,7 @@ def transform(data, *args, **kwargs):
     Returns:
         Anything (e.g. data frame, dictionary, array, int, str, etc.)
     """
-    data['duration'] = data.tpep_dropoff_datetime - data.tpep_pickup_datetime
-    data.duration = data.duration.dt.total_seconds() / 60
-
-    data = data.loc[(data.duration >= 1) & (data.duration <= 60), :]
-
-    categorical = ['PULocationID', 'DOLocationID']
-    data[categorical] = data[categorical].astype(str)
-
-    return data
+    return clean(data)
 
 
 @test
